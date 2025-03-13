49d115870cb5e2a2ac5776acfa2ecc3f93ca1be9
Adarshjithu77 <adarshjithu77@gmail.com>
2025-03-13 14:04:48 +0530

    first commit

--- space.py
+++ space.py
@@ -33,18 +33,61 @@
 #         sum=sum+i
 # print("sum",sum)
    
-a=int(input("enter starting"))
-b=int(input("enter ending"))
-sum1=0
-sum2=0
-sum3=0
-for i in range(a,b+1):
-    if i%2==0:
-        sum1+=i
-    else:
-        sum2+=i    
-else:
-    sum3+=i
-    print(sum1,sum2,sum3)
+# a=int(input("enter starting"))
+# b=int(input("enter ending"))
+# sum1=0
+# sum2=0
+# sum3=0
+# for i in range(a,b+1):
+#     if i%2==0:
+#         sum1+=i
+#     else:
+#         sum2+=i
+#     sum3=sum3+i
+# print(sum1,sum2,sum3)
+  
 
-   
+# a=int(input("enter a number:"))
+# prd=1
+# for i in range(1,a+1):
+#     prd=prd*i
+# print("prd",prd)
+# a=int(input("enter a first number:"))
+# b=int(input("enter a second number"))
+# for i in range(a,b+1):
+#     if i%3==0:
+#         print(i)
+
+# a=int(input("enter a number: "))
+# for i in range(1,11):
+#     print(i,"*",a,"=",i*a)
+# a="python"
+# for i in a:
+#     print(i)
+# rev=''
+# a="python"
+# for i in a:
+#     rev=i+rev
+# print(rev)
+# a=int(input("enter first number:"))
+# b=int(input("enter second number:"))
+# if a>b:
+#     print("smaller number is:",b)
+# else:
+#     print("smaller number is:",a)
+# a=int(input("enter a number:"))
+# if a%2==0 and a%3==0:
+#     print("a is divisble by 2 and 3")
+# else:
+#     print("a is not divisible by 2 and 3")
+# a=int(input("enter a number:"))
+# if a>=90:
+#     print("grade is A")
+# elif a>=80:
+#     print("grade is b")
+# elif a>=70:
+#     print("grade is c")
+# elif a>=60:
+#     print("grade is D ")
+# else:
+#     print("it is not eligible for studies")
