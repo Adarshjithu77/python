d8918ec5245430a1816e67925424ab27514507d1
Adarshjithu77 <adarshjithu77@gmail.com>
2025-03-15 13:26:16 +0530

    first commit

---
+++ example4.py
@@ -0,0 +1,6 @@
+Roll_no=12
+name='sanu'
+weight=56
+print("Roll_no")
+print("name")
+print("weight")
---
+++ prgs.py
@@ -0,0 +1,71 @@
+# a=10
+# b=20
+# print(a+b)
+# a=[1,2,3]
+# b=[2,4,6]
+# print(a+b)
+# a=10
+# a+=20
+# print(a)
+# a=20
+# a-=10
+# print(a)
+# a=10
+# a*=10
+# print(a)
+# a=10
+# a/=3
+# print(a)
+# a=10
+# a//=3
+# print(a)
+# a=10
+# b=20
+# print(a==b)
+# a=10
+# b=20
+# print(a<b)
+# a=10
+# b=20
+# print(a>b)
+# a=10
+# b=20
+# print(a<=b)
+# a=10
+# b=20
+# print(a>=b)
+# a=[1,2,3]
+# b=[1,2,3]
+# print(a)
+# a=[1,2,3]
+# b=[1,2,3]
+# print(b)
+# a=[1,2,3]
+# b=[1,2,3]
+# print(a is b)
+# a=[1,2,3]
+# b=[1,2,3]
+# print(a is not b)
+# a='hello'
+# print('e'in a)
+# print('o'in a)
+# a=10
+# b=11
+# if a==b:
+#     print('equal')
+# a=10
+# b=11
+# if a==b:
+#     print(' equal')
+# else:
+#     print('not equal')
+
+a=10
+b=5
+if a>b:
+    if a<20:
+        print('value greater than 5 and less than 20')
+    else:
+        print('value greater than 5 and not less than 20')
+else:
+    print('not greater than 5')
