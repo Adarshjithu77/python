3d734f07f74760a3c2c80c3ef290b201a0a77463
Adarshjithu77 <adarshjithu77@gmail.com>
2025-03-18 13:31:41 +0530

    first commit

--- nestedloop.py
+++ nestedloop.py
@@ -46,10 +46,51 @@
 # a=3
 # for i in range(4):
 #     for j in range(1,a+1):
-#          if i%2==0:
+#          if i%2==0: #odd row and even row
 #              print(j-1,end=" ")
 #          else:
 #              print(a-j,end=" ")
 #     print()
-        
-    
+# for i in range(1,4):
+#     for j in range(i):
+#         print(j+1,end=" ")
+#     print()    
+
+# for i in range(1,4):
+#     for j in range(i):
+#         print(i,end=" ")
+#     print()    
+
+# 
+# a=1
+# for i in range(1,4):
+#     for j in range(i):
+#         print(a,end=" ")
+#         a+=1
+#     print()
+# a=1
+# for i in range(1,4):
+#     for j in range(i):
+#         print(a,end=" ")
+#         a+=2
+#     print()
+# a=3
+# for i in range(5):
+#     for j in range(i):
+#         # print(j,end=" ")
+#         if i%2==1:
+#              print(j,end=" ")
+#         else:
+#              print(i-j-1,end=" ")
+#     print()
+# for i in range(3):
+#     A=65
+#     for j in range(3):
+#         print(chr(A),end=" ")
+#         A+=1
+#     print()
+# A=65
+# for i in range(3):
+#     for j in range(3):
+#         print(chr(A+i),end=" ")
+#     print()
