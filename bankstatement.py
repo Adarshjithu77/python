35015dd50b3303709a5a2ba836c461f761243d7a
Adarshjithu77 <adarshjithu77@gmail.com>
2025-04-05 13:29:42 +0530

    first commit

---
+++ bankstatement.py
@@ -0,0 +1,14 @@
+bank=[]
+while True:
+    print("""
+1.register
+2.login
+3.exit
+ """   )
+    ch=int(input("enter your choice:"))
+    if ch ==1:
+        if len(bank)==0:
+            bnk=1
+
+
+    
---
+++ new2.py
@@ -0,0 +1,69 @@
+# loop control statement
+# #
+#  for i in range(10):
+# #     print(i)
+# # # # # #     if i==5:
+# # # # # #         break
+# # # # # # print('hai')
+# # # # for i in range(10):
+# # # #     if i==5:
+# # # #         continue
+# # # #     print(i)
+# # # def data():
+# # #     print('hello')
+# # #     print('world')
+# # #     print('python')
+
+# # # data()
+# # # print("function completed")
+# # # data()
+# # # print("fun completed")
+# # # data()
+# # a=10 #global variable
+# # def data():
+# #     b=20  #loacal variable
+# #     print('hello',a)
+# #     print('hello',b)
+
+# # print('before fun ',a)
+# # data()
+# # print('after fun', a)
+# a=10
+# def data():
+#     b=20
+#     a=25
+#     global c
+#     c=5
+#     print("hello",a)
+#     print("hello",b)
+
+# print("before fun ",a)
+# data()
+# print('after fun',c)
+# def demo():
+#     return 'hello','world'
+
+# a,b=demo()
+# print(a)
+# def data(a,b):
+#     print(a)
+
+# data("hello",123)
+
+
+# def data(name,age):
+#     print('name',name)
+#     print('age',age)
+
+# data('manu',20)
+# data('ram',22)
+
+# data(age=20,name='akhil')
+
+
+# def data(name,age=20):
+#     print('name',name)
+#     print('age',age)
+
+# data(name='akhil')
+# data('akhil',22)
